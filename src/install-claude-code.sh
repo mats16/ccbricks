31279c29c55ac061aa09277dedf9defb26fdc23f
mats <mats.kazuki@gmail.com>
2025-11-07 12:06:18 +0900

    supprt shared cluster

--- src/install-claude-code.sh
+++ src/install-claude-code.sh
@@ -4,10 +4,17 @@ set -e
 
 # Install Claude Code
 curl -fsSL https://claude.ai/install.sh | bash -s stable
+
+# Copy to /usr/local
 cp -r $HOME/.local/share/claude /usr/local/share/claude
+chmod -R 755 /usr/local/share/claude
 ln -s /usr/local/share/claude/versions/* /usr/local/bin/claude
 
 # Settings
-echo 'export ANTHROPIC_MODEL="databricks-claude-sonnet-4-5"' >> ~/.bashrc
-echo 'export ANTHROPIC_BASE_URL="$DATABRICKS_HOST/serving-endpoints/anthropic"' >> ~/.bashrc
-echo 'export ANTHROPIC_AUTH_TOKEN="$DATABRICKS_TOKEN"' >> ~/.bashrc
+cat <<EOF > /etc/profile.d/databricks_claude_code.sh
+export ANTHROPIC_MODEL="databricks-claude-sonnet-4-5"
+export ANTHROPIC_BASE_URL="\$DATABRICKS_HOST/serving-endpoints/anthropic"
+export ANTHROPIC_AUTH_TOKEN="\$DATABRICKS_TOKEN"
+EOF
+
+chmod 644 /etc/profile.d/databricks_claude_code.sh
